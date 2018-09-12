4f6c3361f7c1d1c123909ef7e51cb92936a01398
Oleg Ryazanov <weery@live.ru>
2018-09-12 09:46:44 +0300

    Pull request changes

--- src/main/java/net/tngroup/acrestnode/web/components/JsonComponent.java
+++ src/main/java/net/tngroup/acrestnode/web/components/JsonComponent.java
@@ -2,22 +2,15 @@ package net.tngroup.acrestnode.web.components;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JsonComponent {
 
-
+    @Getter
     private ObjectMapper objectMapper;
 
-    public ObjectMapper getObjectMapper() {
-        return objectMapper;
-    }
-
-
-
-    @Autowired
     public JsonComponent() {
         objectMapper = new ObjectMapper();
     }
