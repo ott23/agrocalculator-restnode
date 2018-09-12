da3805db5a8caf3394ca40a650cd892798a8b72f
Oleg Ryazanov <weery@live.ru>
2018-09-12 15:03:40 +0300

    Test passed with init mocks

--- src/test/java/net.tngroup.acrestnode/controllersTest/GeozoneControllerTest.java
+++ src/test/java/net.tngroup.acrestnode/controllersTest/GeozoneControllerTest.java
@@ -1,6 +1,7 @@
 package net.tngroup.acrestnode.controllersTest;
 
 
+import net.tngroup.acrestnode.databases.cassandra.models.Client;
 import net.tngroup.acrestnode.databases.cassandra.services.ClientService;
 import net.tngroup.acrestnode.databases.cassandra.services.GeozoneService;
 import net.tngroup.acrestnode.web.components.JsonComponent;
@@ -9,12 +10,15 @@ import net.tngroup.acrestnode.web.controllers.Responses;
 import net.tngroup.acrestnode.web.security.components.SecurityComponent;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.function.Function;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verifyZeroInteractions;
 
 
@@ -22,23 +26,19 @@ import static org.mockito.Mockito.verifyZeroInteractions;
 public class GeozoneControllerTest {
 
 
+    @InjectMocks
     private GeozoneController geozoneController;
 
-    private JsonComponent jsonComponent;
-    private ClientService clientService;
-    private GeozoneService geozoneService;
-    private SecurityComponent wrongSecurityComponent;
-    private HttpServletRequest httpServletRequest;
+    @Mock private JsonComponent jsonComponent;
+    @Mock private ClientService clientService;
+    @Mock private GeozoneService geozoneService;
+    @Spy private SecurityComponent wrongSecurityComponent = Mockito.spy(new WrongSecurityComponent());
+    @Mock private HttpServletRequest httpServletRequest;
 
     @Before
     public void initMocks(){
+        MockitoAnnotations.initMocks(this);
 
-        jsonComponent = mock(JsonComponent.class);
-        clientService = mock(ClientService.class);
-        geozoneService = mock(GeozoneService.class);
-        wrongSecurityComponent  = next -> Responses.failedDependencyResponse();
-        httpServletRequest = mock(HttpServletRequest.class);
-        geozoneController = new GeozoneController(clientService, geozoneService, jsonComponent, wrongSecurityComponent);
     }
 
     @Test
@@ -52,5 +52,11 @@ public class GeozoneControllerTest {
         verifyZeroInteractions(geozoneService);
     }
 
+    private class WrongSecurityComponent implements SecurityComponent{
+        @Override
+        public ResponseEntity doIfUser(Function<Client, ResponseEntity> next) {
+            return Responses.failedDependencyResponse();
+        }
+    }
 
 }
