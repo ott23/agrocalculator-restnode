cf4d81a9b662872d78edf4114a0a0c942c1cfacd
Oleg Ryazanov <weery@live.ru>
2018-09-13 15:48:14 +0300

    minor refactoring

--- src/main/java/net/tngroup/acrestnode/web/controllers/TaskController.java
+++ src/main/java/net/tngroup/acrestnode/web/controllers/TaskController.java
@@ -2,7 +2,6 @@ package net.tngroup.acrestnode.web.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import net.tngroup.acrestnode.databases.cassandra.models.Client;
 import net.tngroup.acrestnode.databases.cassandra.models.TaskCondition;
 import net.tngroup.acrestnode.databases.cassandra.models.TaskKey;
 import net.tngroup.acrestnode.databases.cassandra.models.TaskResult;
@@ -11,12 +10,12 @@ import net.tngroup.acrestnode.databases.cassandra.services.TaskConditionService;
 import net.tngroup.acrestnode.databases.cassandra.services.TaskResultService;
 import net.tngroup.acrestnode.web.components.JsonComponent;
 import net.tngroup.acrestnode.web.components.KafkaComponent;
+import net.tngroup.acrestnode.web.controllers.requestmodels.PollRequest;
 import net.tngroup.acrestnode.web.controllers.requestmodels.TaskRequest;
 import net.tngroup.acrestnode.web.security.components.SecurityComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -58,16 +57,12 @@ public class TaskController {
     Метод отправки в Kafka
      */
     @RequestMapping(value = "/send", method = RequestMethod.POST)
-    public ResponseEntity send(HttpServletRequest request, @RequestBody TaskRequest taskRequest) throws IOException {
+    public ResponseEntity send(HttpServletRequest request, @RequestBody TaskRequest taskRequest) {
 
         return securityComponent.doIfUser(client -> {
 
             // Connection with Kafka testing
-            try {
-                kafkaComponent.testSocket();
-            } catch (Exception e) {
-                return kafkaNotAvailableResponse();
-            }
+            if (testKafkaConnextion()) return kafkaNotAvailableResponse();
 
             final String topic = taskRequest.getTopic();
             final String message = taskRequest.getMessage();
@@ -109,28 +104,17 @@ public class TaskController {
 
 
     @RequestMapping(value = "/poll", method = RequestMethod.POST)
-    public ResponseEntity poll(HttpServletRequest request, @RequestBody String jsonRequest) throws IOException {
-        // Get client, error if null
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
-        Client client = clientService.getByName(name);
-        if (client == null) return failedDependencyResponse();
+    public ResponseEntity poll(HttpServletRequest request, @RequestBody PollRequest pollRequest) throws IOException {
 
-        // Connection with Kafka testing
-        try {
-            kafkaComponent.testSocket();
-        } catch (Exception e) {
-            return kafkaNotAvailableResponse();
-        }
+        return securityComponent.doIfUser(client -> {
+            if (testKafkaConnextion()) return kafkaNotAvailableResponse();
 
-        // Json parsing
-        ObjectNode json = (ObjectNode) jsonComponent.getObjectMapper().readTree(jsonRequest);
-        UUID task = UUID.fromString(json.remove("task").asText());
+            final TaskKey taskKey = new TaskKey(client.getId(), pollRequest.getTask());
+            final TaskResult taskResult = taskResultService.getByKey(taskKey);
 
-        TaskKey taskKey = new TaskKey(client.getId(), task);
-        TaskResult taskResult = taskResultService.getByKey(taskKey);
-
-        if (taskResult == null) return okResponse(formTaskNotReady(task));
-        else return okResponse(formTaskResult(taskResult));
+            if (taskResult == null) return okResponse(formTaskNotReady(pollRequest.getTask()));
+            else return okResponse(formTaskResult(taskResult));
+        });
     }
 
     private void sleep(int time) {
@@ -169,5 +153,14 @@ public class TaskController {
         response.put("result", "No result available");
 
         return response.toString();
+    }
+
+    private boolean testKafkaConnextion() {
+        try {
+            kafkaComponent.testSocket();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
