c73ca06cb36f15158f28af99e6524d74ab9493b8
Oleg Ryazanov <weery@live.ru>
2018-09-19 12:51:39 +0300

    Implemented GeozoneControllerTests as ClientEntityTest child. Passed.

--- src/test/java/net.tngroup.acrestnode/controllersTest/GeozoneControllerTest.java
+++ src/test/java/net.tngroup.acrestnode/controllersTest/GeozoneControllerTest.java
@@ -1,24 +1,23 @@
 package net.tngroup.acrestnode.controllersTest;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.tngroup.acrestnode.databases.cassandra.models.Geozone;
-import net.tngroup.acrestnode.databases.cassandra.services.ClientService;
 import net.tngroup.acrestnode.databases.cassandra.services.GeozoneService;
+import net.tngroup.acrestnode.databases.cassandra.services.base.ClientEntityService;
 import net.tngroup.acrestnode.web.components.JsonComponent;
 import net.tngroup.acrestnode.web.controllers.GeozoneController;
 import net.tngroup.acrestnode.web.controllers.Responses;
+import net.tngroup.acrestnode.web.controllers.base.ClientEntityController;
 import net.tngroup.acrestnode.web.security.components.SecurityComponent;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.*;
-import org.springframework.http.HttpStatus;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
@@ -27,7 +26,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 
-public class GeozoneControllerTest {
+public class GeozoneControllerTest extends ClientEntityControllerTest<Geozone> {
 
 
     private final static UUID MOCK_CLIENT_ID = UUID.randomUUID();
@@ -38,53 +37,12 @@ public class GeozoneControllerTest {
     @Mock
     private JsonComponent jsonComponent;
     @Mock
-    private ClientService clientService;
-    @Mock
     private GeozoneService geozoneService;
     @Spy
-    private SecurityComponent securityComponent;
+    private SecurityComponent securityComponent = new ValidSecurityComponent(MOCK_CLIENT_ID);
     @Mock
     private HttpServletRequest httpServletRequest;
 
-    @Before
-    public void initValidSecurityComponent() {
-        securityComponent = Mockito.spy(new ValidSecurityComponent(MOCK_CLIENT_ID));
-    }
-
-    //region ==================== GetList ====================
-
-    @Test
-    public void givenNullClient_whenCallGetList_thenShouldBeCallGeozoneService() {
-
-        securityComponent = Mockito.spy(new WrongSecurityComponent());
-        MockitoAnnotations.initMocks(this);
-
-        HttpStatus resultStatus = geozoneController.getList(httpServletRequest).getStatusCode();
-
-        assertEquals(resultStatus, HttpStatus.FAILED_DEPENDENCY);
-
-        verifyZeroInteractions(geozoneService);
-    }
-
-    @Test
-    public void givenClient_whenCallGetList_thenShouldBeCallGeozoneServiceAndReturnResponse() {
-
-        MockitoAnnotations.initMocks(this);
-
-        List<Geozone> result = new ArrayList<>();
-
-        when(geozoneService.getAllByClient(any())).thenReturn(result);
-
-        ResponseEntity actual = geozoneController.getList(httpServletRequest);
-
-        verify(geozoneService, times(1)).getAllByClient(any());
-
-        assertEquals(actual.getStatusCode(), HttpStatus.OK);
-
-        assertEquals(actual.getBody(), result);
-    }
-
-    //endregion
 
     //region ==================== Save ====================
 
@@ -125,126 +83,42 @@ public class GeozoneControllerTest {
         );
     }
 
-    //endregion
-
-    //region ==================== Get ====================
-
-    @Test
-    public void givenValidClientIdAndGeozoneId_whenCallGet_thenShouldBeReturnGeozone() throws JsonProcessingException {
-
-        MockitoAnnotations.initMocks(this);
-
-        final Geozone mockGeozone = new Geozone();
-        final ObjectMapper objectMapper = new ObjectMapper();
-        mockGeozone.setClient(MOCK_CLIENT_ID);
-
-        when(geozoneService.getById(any())).thenReturn(mockGeozone);
-        when(jsonComponent.getObjectMapper()).thenReturn(objectMapper);
-
-        assertEquals(
-                geozoneController.getById(httpServletRequest, UUID.randomUUID()).getBody(),
-                objectMapper.writeValueAsString(mockGeozone));
-
-
+    @Override
+    protected ClientEntityController<Geozone> getEntityController() {
+        return geozoneController;
     }
 
-    @Test
-    public void givenNonExistedID_whenCallGet_thenShouldBeReturnNotFoundResponse() {
-
-        MockitoAnnotations.initMocks(this);
-
-        when(geozoneService.getById(any())).thenReturn(null);
-
-        assertEquals(
-                Responses.nonFoundResponse().getStatusCode(),
-                geozoneController.getById(httpServletRequest, UUID.randomUUID()).getStatusCode()
-        );
-
+    @Override
+    protected void setSecurityComponent(SecurityComponent securityComponent) {
+        this.securityComponent = securityComponent;
     }
 
-    @Test
-    public void givenGeozoneIdFromOtherClient_whenCallGet_thenShouldBeReturnFailedDependency() {
+    @Override
+    protected HttpServletRequest getHttpServletMock() {
+        return httpServletRequest;
+    }
 
+    @Override
+    protected ClientEntityService<Geozone> getClientEntityService() {
+        return geozoneService;
+    }
+
+    @Override
+    protected JsonComponent getJsonComponent() {
+        return jsonComponent;
+    }
+
+    @Override
+    protected Geozone newEntity() {
+        return new Geozone();
+    }
+
+    @Override
+    protected void initMocks() {
         MockitoAnnotations.initMocks(this);
-
-        final Geozone mockGeozone = new Geozone();
-        mockGeozone.setClient(UUID.randomUUID());
-
-        when(geozoneService.getById(any())).thenReturn(mockGeozone);
-        assertEquals(
-                geozoneController.getById(httpServletRequest, UUID.randomUUID()).getStatusCode(),
-                Responses.failedDependencyResponse().getStatusCode()
-        );
-
     }
 
     //endregion
 
-    //region ==================== Delete ====================
 
-    @Test
-    public void givenValidGeozoneIdAndClientId_whenCallDelete_thenShouldBeReturnSuccessResponse() {
-
-        MockitoAnnotations.initMocks(this);
-
-        final Geozone geozone = new Geozone();
-        geozone.setClient(MOCK_CLIENT_ID);
-
-        when(geozoneService.getById(any())).thenReturn(geozone);
-        when(geozoneService.deleteById(any())).thenReturn(true);
-
-        assertEquals(
-                geozoneController.deleteById(httpServletRequest, UUID.randomUUID()),
-                Responses.successResponse()
-        );
-
-    }
-
-    @Test
-    public void givenInvalidGeozoneId_whenCallDelete_thenShouldBeReturnNotFound() {
-
-        MockitoAnnotations.initMocks(this);
-        when(geozoneService.getById(any())).thenReturn(null);
-
-        assertEquals(
-                geozoneController.deleteById(httpServletRequest, UUID.randomUUID()),
-                Responses.nonFoundResponse()
-        );
-    }
-
-    @Test
-    public void givenInvalidClientId_whenCallDelete_thenShouldBeReturnFailedDependency() {
-
-        MockitoAnnotations.initMocks(this);
-
-        final Geozone geozone = new Geozone();
-        geozone.setClient(UUID.randomUUID());
-
-        when(geozoneService.getById(any())).thenReturn(geozone);
-
-        assertEquals(
-                geozoneController.deleteById(httpServletRequest, UUID.randomUUID()),
-                Responses.failedDependencyResponse()
-        );
-    }
-
-    @Test
-    public void givenValidClientIdAndGeozoneId_whenTwoClientDoDeletePermanent_thenReturnNonFound() {
-
-        MockitoAnnotations.initMocks(this);
-
-        final Geozone geozone = new Geozone();
-        geozone.setClient(MOCK_CLIENT_ID);
-
-        when(geozoneService.getById(any())).thenReturn(geozone);
-        when(geozoneService.deleteById(any())).thenReturn(false);
-
-        assertEquals(
-                geozoneController.deleteById(httpServletRequest, UUID.randomUUID()),
-                Responses.nonFoundResponse()
-        );
-
-    }
-
-    //endregion
 }
